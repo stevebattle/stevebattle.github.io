5eac52d253f5b21013e5294455cc00c34236c00f
Steve Battle <Steve.Battle@uwe.ac.uk>
2016-04-07 14:21:29 +0100

    control spacing

--- supershapes/sketch.js
+++ supershapes/sketch.js
@@ -3,7 +3,7 @@
   http://paulbourke.net/geometry/supershape/
  */
 var aSlider, gSlider, bSlider;
-var x=100, y = 2, yStep = 30;
+var x=100, y = 30, yStep = 35;
 var label = [], l=0;
 var textHeight = 15;
 
