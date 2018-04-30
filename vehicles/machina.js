856cd7b9bf423b8303f7d99cb43426e511c5d7ff
Steve Battle <steve.battle@uwe.ac.uk>
2018-04-30 22:16:39 +0100

    epicyclic

--- vehicles/machina.js
+++ vehicles/machina.js
@@ -1,4 +1,4 @@
- /*
+/*
    Interactive ELSIE
  
    Copyright 2018 Steve Battle
@@ -13,6 +13,11 @@
 var RATE = 30;
 // 1751 × 1246 image
 var V1WIDTH=30, VLENGTH=85, VWIDTH=(1246/1751.0)*VLENGTH, VSIDE=50;
+
+// obstacle dimension
+var OSIDE = 60;
+var obs_visible = true;
+var lastX, lastY;
 
 function preload() {
   bg_image = loadImage("data/bg800x800.jpg");
@@ -51,13 +56,33 @@ function draw() {
 }
 
 function mouseClicked() {
+  if (mouseX==lastX && mouseY==lastY) return false;
+  lastX = mouseX; lastY = mouseY;
+
   // delete existing obstacle?
   for (var i=0; i<obs.length; i++) {
     if (obs[i].encloses(mouseX,mouseY)) {
       obs.splice(i,1);
-      return;
+      return false;
     }
   }
   // otherwise create new obstacle
-  obs[obs.length] = new Obstacle(obs_image,OSIDE,mouseX,mouseY);  
+  obs[obs.length] = new Obstacle(obs_image,OSIDE,mouseX,mouseY);
+  return false;
+}
+
+function touchEnded() {
+  if (mouseX==lastX && mouseY==lastY) return false;
+  lastX = mouseX; lastY = mouseY;
+
+  // delete existing obstacle?
+  for (var i=0; i<obs.length; i++) {
+    if (obs[i].encloses(mouseX,mouseY)) {
+      obs.splice(i,1);
+      return false;
+    }
+  }
+  // otherwise create new obstacle
+  obs[obs.length] = new Obstacle(obs_image,OSIDE,mouseX,mouseY);
+  return false; 
 }
