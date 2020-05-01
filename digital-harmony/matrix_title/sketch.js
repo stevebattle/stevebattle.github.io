aeefd8ae1c0a021fcea484ba4b3b4b5ade512c6c
Steve Battle <Steve.Battle@uwe.ac.uk>
2020-05-01 14:40:25 +0100

    matrix rebooted

--- digital-harmony/matrix_title/sketch.js
+++ digital-harmony/matrix_title/sketch.js
@@ -1,6 +1,7 @@
 function setup() {
   canvas = createCanvas(500,500) ;
   icebreaker = loadFont("data/Icebreaker.otf", 32);
+  frameRate = 30;
 }
 
 function draw() {
@@ -8,7 +9,10 @@ function draw() {
   background(0) ;
   textSize(100) ;
   textFont(icebreaker);
-  fill('magenta') ;
+  fill('red') ;
   noStroke();
   text("MATRIX",45,200) ;
+  text("III",200,300) ;
+  textSize(50) ;
+  if (frameCount>90) text("rebooted",140,400)
 }
