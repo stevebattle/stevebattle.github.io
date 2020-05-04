4ab1b671543531623d51ad972829e57bb5edcf3a
Steve Battle <Steve.Battle@uwe.ac.uk>
2020-05-04 10:21:35 +0100

    matrix hex

--- digital-harmony/matrix_hex/matrix_hex.js
+++ digital-harmony/matrix_hex/matrix_hex.js
@@ -2,21 +2,19 @@
 // https://www.dataisnature.com/?p=2274
 // https://archive.org/stream/DigitalHarmony_201611/Digital%20Harmony#mode/1up
 
-var npoints = 24 ; // number of points in a display
-var steps = 20 ; // step at the last frame
-var side = 100 ;
+var npoints = 24 ; // number of points in the display
+var rate = 20 ; // draw is called at 20Hz
+var minSide = 10;
+var maxSide = 100 ;
 var radius = 150 ;
 var xcenter, ycenter ;
-var startTime = 25;
 
 function setup() {
   canvas = createCanvas(500,500) ;
   background(0) ;
-  // draw is called at 24Hz
-  frameRate(24) ;
+  frameRate(rate) ;
   xcenter = width/2 ;
   ycenter = height/2 ;
-  noFill();
 }
 
 // source: https://processing.org/examples/regularpolygon.html
@@ -34,29 +32,39 @@ function polygon(x, y, radius, npoints) {
 
 // lissajous figure
 
-function lissajous(a,b,t,delta,r,s) {
+function lissajous(a,b,t,delta,r,f) {
   // delta provides an offset to animate the lissajous
   var x = r * sin(a*t + delta) + xcenter ;    
   var y = r * sin(b*t) + ycenter ; 
-  // hexagon
-  polygon(x,y,s,6) ;
+  f(x,y);
 }
 
 function draw() {
-  // lissajous parameters "figure of 8"
-  //a=2, b=1 ;
-  a=3, b=2 ;
+  // lissajous parameters
+  var a=3, b=2 ;
 
   // black background
   background(0) ;
   stroke('white') ;
 
-  // step in overall cycle (1 = full cycle)
-  var time = (frameCount-1) / steps +startTime;
+  // time = seconds into the animation
+  var time = frameCount / rate ;
   
+  side = min(max(floor(5*time-90),minSide),maxSide);
+
   for (var p=1; p<=npoints; p++) {
-    // t indicates position along lissajous
+    // t is position along lissajous
     var t = p / npoints * time ;    
-    lissajous(a,b,t,0,radius,p/npoints*side) ;
+    lissajous(a,b,t,0,radius, function(x,y) { 
+      // circle or hexagon
+      if (side==minSide) {
+        fill(255);
+        ellipse(x,y,10,10) ;
+      }
+      else {
+        noFill();
+        polygon(x,y,p/npoints*side,6) ;
+      }
+    }) ;
   }
 }
